30bb45c08110beffce79c7f07a4372b87a23ef1b
Mai Xuân Hiếu <maixuanhieu250103@gmail.com>
2025-08-13 12:34:07 +0700

    Configure auto-lint and format pre-commit hooks

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-    { ignores: ['dist'] },
+    { ignores: ['dist', 'node_modules', '.husky'] },
     {
         extends: [js.configs.recommended, ...tseslint.configs.recommended],
         files: ['**/*.{ts,tsx}'],
@@ -20,6 +20,10 @@ export default tseslint.config(
         rules: {
             ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            '@typescript-eslint/no-explicit-any': 'warn',
+            'prefer-const': 'error',
+            'no-var': 'error',
         },
     }
 );
