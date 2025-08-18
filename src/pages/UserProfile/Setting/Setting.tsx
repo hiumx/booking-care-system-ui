c599205f26f1c93608ad675f6ce7a9557d8fb0ef
DevHuy44 <kieuvanhuy813@gmail.com>
2025-08-18 15:56:44 +0700

    [FIX] Implement update UI setting user profile and scss authLayoutContainer in AuthLayout.scss

--- src/pages/UserProfile/Setting/Setting.tsx
+++ src/pages/UserProfile/Setting/Setting.tsx
@@ -21,36 +21,34 @@ const Setting: React.FC<SettingProps> = ({
 
     return (
         <>
-            <div className="col-lg-8 col-xl-9">
-                <nav className="settings-tab mb-1">
-                    <ul className="nav nav-tabs-bottom" role="tablist">
-                        <li className="nav-item" role="presentation">
-                            <button
-                                className={`nav-link ${activeTab === 'profile' ? 'active' : ''}`}
-                                onClick={() => handleTabClick('profile')}
-                                type="button"
-                            >
-                                Thông tin cá nhân
-                            </button>
-                        </li>
-                        <li className="nav-item" role="presentation">
-                            <button
-                                className={`nav-link ${activeTab === 'password' ? 'active' : ''}`}
-                                onClick={() => handleTabClick('password')}
-                                type="button"
-                            >
-                                Thay đổi mật khẩu
-                            </button>
-                        </li>
-                    </ul>
-                </nav>
-                <div className="card">
-                    <div className="card-body">
-                        <div className="border-bottom pb-3 mb-3">
-                            <h5>{title}</h5>
-                        </div>
-                        {children}
+            <nav className="settings-tab mb-1">
+                <ul className="nav nav-tabs-bottom" role="tablist">
+                    <li className="nav-item" role="presentation">
+                        <button
+                            className={`nav-link ${activeTab === 'profile' ? 'active' : ''}`}
+                            onClick={() => handleTabClick('profile')}
+                            type="button"
+                        >
+                            Thông tin cá nhân
+                        </button>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                        <button
+                            className={`nav-link ${activeTab === 'password' ? 'active' : ''}`}
+                            onClick={() => handleTabClick('password')}
+                            type="button"
+                        >
+                            Thay đổi mật khẩu
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+            <div className="card">
+                <div className="card-body">
+                    <div className="border-bottom pb-3 mb-3">
+                        <h5>{title}</h5>
                     </div>
+                    {children}
                 </div>
             </div>
         </>
